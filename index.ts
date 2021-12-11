c21ca2c7eb6febfea30a21918e038f346abfe8c9
Pavel Rakhaev <rakhaevpavel@gmail.com>
2021-12-12 00:37:20 +0300

    Optimize TS file

--- index.ts
+++ index.ts
@@ -1,17 +1,23 @@
 import axios from "axios";
 
-const url = "jsonplaceholder.typicode.com/todos/1";
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 axios.get(url).then(res => {
-  const todo = res.data;
+  const { id, title, completed } = res.data as Todo;
 
-  const ID = todo.id;
-  const title = todo.title;
-  const finished = todo.finished;
-
-  console.log(`
-    The Todo with ID: ${ID}
-    Has a title of: ${title}
-    Is it finished? ${finished}
-  `);
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean): void => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it completed? ${completed}
+  `);
+};
