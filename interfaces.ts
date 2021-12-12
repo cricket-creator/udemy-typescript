ff1f461abec3c77762595b0ab13285c215cb4331
Pavel Rakhaev <rakhaevpavel@gmail.com>
2021-12-12 17:21:05 +0300

    Interfaces in TS

---
+++ interfaces.ts
@@ -0,0 +1,47 @@
+interface Reportable {
+  /*name: string;
+  year: Date;
+  broken: boolean;*/
+
+  summary(): string;
+}
+
+const oldCivic = {
+  name: "civic",
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
+};
+
+const cola = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `Cola has ${this.sugar} grams of sugar`;
+  }
+};
+
+/*const printVehicle = (vehicle: { name: string; year: number; broken: boolean; }): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken?: ${vehicle.broken}`);
+};*/
+
+/*const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken?: ${vehicle.broken}`);
+};*/
+
+const printSummary = (item: Reportable): void => {
+  // console.log(`Name: ${vehicle.name}`);
+  // console.log(`Year: ${vehicle.year}`);
+  // console.log(`Broken?: ${vehicle.broken}`);
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(cola);
